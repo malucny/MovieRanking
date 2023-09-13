a816a58176b089352b4fdd5aa153c401aff6b205
Maria Luísa <malucnydev@gmail.com>
2023-09-13 19:51:33 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang = "pt-br">
+
+<head>
+    <title>Ranking de Filmes</title>
+	<meta charset="UTF-8"/>
+	<link rel= "stylesheet" href="estilos/estilos.css"/>
+
+</head>
+<body>
+    <div id="corpo">
+	<h1>Escolha seu filme</h1>
+	<table class="listagem">
+	<tr><td>Foto<td>Nome<td>Adm
+	<tr><td>Foto<td>Nome<td>Adm
+	<tr><td>Foto<td>Nome<td>Adm
+	<tr><td>Foto<td>Nome<td>Adm
+	</table>
+	</div>
+
+</body>
+</html>
